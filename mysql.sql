a7c477d62b14db474dc40f052551bd713060b853
庄表伟 <zhuangbiaowei@huawei.com>
2018-12-08 11:48:20 +0800

    Create mysql.sql

---
+++ mysql.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE kaiyuanshe;
+
+use kaiyuanshe;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `wechat_id` varchar(255) NOT NULL,
+  `nick_name` varchar(255) NOT NULL,
+  `work_group` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `github_name` varchar(255),
+  `introduce`  varchar(3000),
+  `status` varchar(100),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
+
+
+CREATE TABLE IF NOT EXISTS `wechat_friends` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `wechat_id` varchar(255) NOT NULL,
+  `nick_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
