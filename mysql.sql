944798f7d14043456e4cfb923af6027da2d2f158
kaiyuanshe-robot <zbw25@hotmail.com>
2018-12-18 08:47:24 +0800

    添加推荐人字段

--- mysql.sql
+++ mysql.sql
@@ -9,6 +9,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `nick_name` varchar(255) NOT NULL,
   `work_group` varchar(255) NOT NULL,
   `position` varchar(255) NOT NULL,
+  `referee1` varchar(255),
+  `referee2` varchar(255),
   `github_name` varchar(255),
   `introduce`  varchar(3000),
   `status` varchar(100),
