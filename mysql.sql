14ff672d3a14eb106ff244c2469536053c3bedbf
Zhuang BiaoWei <zhuangbiaowei@huawei.com>
2018-12-22 16:18:32 +0800

    修改创建时间戳的SQL语句

--- mysql.sql
+++ mysql.sql
@@ -35,5 +35,6 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `mention_id_list` varchar(3000),
   `mention_name_list` varchar(3000),
   `text` varchar(3000),
-  `datatime` datatime NOT NULL,
+  `create_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1;
