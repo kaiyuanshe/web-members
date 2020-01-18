ea88d77381c3c0bd55d99a0b33ad43885d0b6c71
David Z <geek@lzw.name>
2020-01-18 18:11:55 +0800

    add ejs-mate and redesign

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const path = require('path');
+const express = require("express");
+const engine = require("ejs-mate");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const path = require("path");
 const app = express();
 
-const {getHomePage} = require('./routes/index');
-const {addVolunteerPage,addMemberPage,addPlayer} = require('./routes/player');
+const { getHomePage } = require("./routes/index");
+const {
+  addVolunteerPage,
+  addMemberPage,
+  addPlayer
+} = require("./routes/player");
 const port = 2000;
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'kys',
-    password: 'kys',
-    database: 'kaiyuanshe'
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "kys",
+  password: "kys",
+  database: "kaiyuanshe"
 });
 
 // connect to database
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
+db.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database");
 });
 global.db = db;
 
 // configure middleware
-app.set('port', process.env.port || port); // set express to use this port
-app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
-app.set('view engine', 'ejs'); // configure template engine
+app.set("port", process.env.port || port); // set express to use this port
+app.engine("ejs", engine);
+app.set("views", __dirname + "/views"); // set express to look in this folder to render our view
+app.set("view engine", "ejs"); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
-app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+app.use(express.static(path.join(__dirname, "public"))); // configure express to use public folder
 app.use(fileUpload()); // configure fileupload
 
 // routes for the app
-app.get('/', getHomePage);
-app.get('/add_volunteer', addVolunteerPage);
-app.get('/add_member', addMemberPage);
-app.post('/add', addPlayer);
+app.get("/", getHomePage);
+app.get("/add_volunteer", addVolunteerPage);
+app.get("/add_member", addMemberPage);
+app.post("/add", addPlayer);
 
 // set the app to listen on the port
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
 module.exports = {
-    getHomePage: (req, res) => {
-        let query = "SELECT * FROM `users` ORDER BY id ASC"; // query database to get all the players
+  getHomePage: (req, res, next) => {
+    let query = "SELECT * FROM `users` ORDER BY id ASC"; // query database to get all the players
 
-        // execute query
-        db.query(query, (err, result) => {
-            if (err) {
-                res.redirect('/');
-            }
-            res.render('index.ejs', {
-                title: "开源社成员管理 | 成员列表"
-                ,players: result
-            });
-        });
-    },
+    // execute query
+    db.query(query, (err, result) => {
+      res.render("index", {
+        title: "开源社成员管理 | 成员列表",
+        players: result
+      });
+    });
+  }
 };
